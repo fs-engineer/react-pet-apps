3f7a03ddf3d9e28bc23e194988cfb42a0030b3ba
Vadym Yevlanov <vad.evlanov@gmail.com>
2023-01-19 12:17:30 +0100

    Configure React app for deployment to GitHub Pages

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ export const CONTACTS_DATA = "contactsData";
 export const ESCAPE = "Escape";
 export const OVERLAY = "overlay";
 export const KEYDOWN = "keydown";
-export const PER_PAGE = 120;
+export const PER_PAGE = 15;
 export const PHOTO = "photo";
 export const HORIZONTAL = "horizontal";
